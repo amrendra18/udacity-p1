0047434a07a65b5c02aaa47d05fa51434846a883
Amrendra Kumar <amrendra.nitb@gmail.com>
2015-12-05 23:12:35 +0530

    Add reviews to detail fragment

--- app/src/main/java/com/amrendra/popularmovies/app/fragments/DetailFragment.java
+++ app/src/main/java/com/amrendra/popularmovies/app/fragments/DetailFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,6 +18,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.amrendra.popularmovies.R;
@@ -50,6 +53,7 @@ public class DetailFragment extends Fragment {
     @Bind(R.id.movie_poster_image)
     ImageView posterImageView;
 
+
     // Overview Card
     @Bind(R.id.detail_overview_card_title)
     TextView movieOverviewTitleTv;
@@ -69,8 +73,31 @@ public class DetailFragment extends Fragment {
 
     @Bind(R.id.detail_orginal_language_tv)
     TextView movieOriginalLanguageTv;
-
     // End Overview Card
+
+
+    // Reviews Card
+    @Bind(R.id.review_progressbar)
+    ProgressBar reviewsProgressbar;
+
+    @Bind(R.id.no_reviews_tv)
+    TextView noReviewsTv;
+
+    @Bind(R.id.reviews_container)
+    LinearLayout reviewsContainer;
+    // End : Reviews Card
+
+
+    // Trailes Card
+    @Bind(R.id.trailer_progressbar)
+    ProgressBar trailerProgressbar;
+
+    @Bind(R.id.no_trailers_tv)
+    TextView noTrailerTv;
+
+    @Bind(R.id.trailer_recyvlerview)
+    RecyclerView trailerRecyclerView;
+    // End : Reviews Card
 
 
     Movie mMovie = null;
@@ -237,13 +264,14 @@ public class DetailFragment extends Fragment {
         @Override
         public Loader<ReviewList> onCreateLoader(int id, Bundle args) {
             Debug.c();
+            reviewsProgressbar.setVisibility(ProgressBar.VISIBLE);
             return new ReviewsLoader(getActivity(), mMovie.id);
         }
 
         @Override
         public void onLoadFinished(Loader<ReviewList> loader, ReviewList data) {
-            Debug.e(data.toString(), false);
             Debug.c();
+            addReviews(data);
         }
 
         @Override
@@ -272,4 +300,70 @@ public class DetailFragment extends Fragment {
             Debug.c();
         }
     };
+
+
+    private void addReviews(ReviewList result) {
+        reviewsProgressbar.setVisibility(ProgressBar.INVISIBLE);
+        Debug.e(result.toString(), false);
+        if (result == null) {
+            noReviewsTv.setText(getResources().getText(R.string.error_detail_reviews));
+            noReviewsTv.setVisibility(View.VISIBLE);
+            return;
+        }
+        final LayoutInflater inflater = LayoutInflater.from(getActivity());
+        List<Review> reviewList = result.results;
+
+        if (!(reviewList == null || reviewList.isEmpty())) {
+            int len = reviewList.size();
+
+
+            for (int i = 0; i < len; i++) {
+                Review review = reviewList.get(i);
+                final View reviewView = inflater.inflate(R.layout.review_layout, reviewsContainer, false);
+                TextView reviewAuthor = ButterKnife.findById(reviewView, R.id.review_by);
+                TextView reviewContent = ButterKnife.findById(reviewView, R.id.review_content);
+                reviewAuthor.setText(review.author);
+                reviewContent.setText(review.content);
+                reviewsContainer.addView(reviewView);
+                if (i < len - 1) {
+                    final View dividerView = inflater.inflate(R.layout.divider_content,
+                            reviewsContainer, false);
+                    View divider = ButterKnife.findById(dividerView, R.id.divider_content);
+                    reviewsContainer.addView(divider);
+                }
+            }
+            noReviewsTv.setVisibility(View.GONE);
+        } else {
+            noReviewsTv.setText(getResources().getText(R.string.no_detail_reviews));
+            noReviewsTv.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+
+    private void addTrailers(TrailerList result) {
+        trailerProgressbar.setVisibility(ProgressBar.INVISIBLE);
+        Debug.e(result.toString(), false);
+/*        if (result == null) {
+            noReviewsTv.setText(getResources().getText(R.string.error_detail_reviews));
+            noReviewsTv.setVisibility(View.VISIBLE);
+            return;
+        }
+        final LayoutInflater inflater = LayoutInflater.from(getActivity());
+        List<Review> reviewList = result.results;
+        if (!(reviewList == null || reviewList.isEmpty())) {
+            for (Review review : reviewList) {
+                final View reviewView = inflater.inflate(R.layout.review_layout, reviewsContainer, false);
+                TextView reviewAuthor = ButterKnife.findById(reviewView, R.id.review_by);
+                TextView reviewContent = ButterKnife.findById(reviewView, R.id.review_content);
+                reviewAuthor.setText(review.author);
+                reviewContent.setText(review.content);
+                reviewsContainer.addView(reviewView);
+            }
+        } else {
+            noReviewsTv.setText(getResources().getText(R.string.no_detail_reviews));
+            noReviewsTv.setVisibility(View.VISIBLE);
+        }*/
+
+    }
 }
